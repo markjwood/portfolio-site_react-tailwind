ddbf0d4b2b2b863a68b996edc5f1dd3d6c9c515d
Mark <mark@markjwood.com>
2022-10-13 10:24:48 -0700

    initial commit; basic site ready

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const projects = [
+	{
+		title: 'Random Quote Generator',
+		subtitle: 'HTML, CSS, Vanilla JavaScript, Responsive design',
+		description: 'Displays a random inspirational quote',
+		image:
+			'./project-screenshots/quote-generator/random-quote-generator_screenshot-main.png',
+		link: 'https://markjwood.github.io/random-quote-generator/',
+	},
+	{
+		title: 'Searchable Student Directory',
+		subtitle: 'HTML, CSS, JavaScript, Responsive design',
+		description:
+			'A searchable list of students. This project displays a paginated list of students that updates in real-time as a search is entered.',
+		image:
+			'./project-screenshots/searchable-student-contact/student-contact_screenshot-3.png',
+		link: 'https://markjwood.github.io/data-pagination-project/',
+	},
+	{
+		title: 'Event Registration Form',
+		subtitle: 'HTML, CSS, JavaScript, Form validation',
+		description:
+			'A sample event registration form. This is a registration form for a fictional conference. Form inputs are validated in real-time (with hints provided as necessary) and options are updated to display only those that are available with currently-selected choices.',
+		image:
+			'./project-screenshots/event-registration-form/registration-form_screenshot-2.png',
+		link: 'https://markjwood.github.io/interactive-form-project/',
+	},
+	{
+		title: 'Phrase Hunter Game',
+		subtitle: 'HTML, CSS, JavaScript, Object-oriented programming',
+		description:
+			'A hangman-style game. Displays blank squares representing the letters in a phrase randomly-selected from a JSON file; as letters are "guessed," they are either displayed or marked as unavailable. Five incorrect guesses or "solving" the phrase ends the game.',
+		image:
+			'./project-screenshots/phrase-hunter-game/phrase-hunter_screenshot-main.png',
+		link: 'https://markjwood.github.io/oop-game-show-app/',
+	},
+];
+
+export const testimonials = [
+	{
+		quote:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+		image: 'https://randomuser.me/api/portraits/men/1.jpg',
+		name: 'Jesse Hicks',
+		company: 'Zoozle',
+	},
+	{
+		quote:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.',
+		image: 'https://randomuser.me/api/portraits/men/94.jpg',
+		name: 'Ruben Alvarez',
+		company: 'Dooble.io',
+	},
+];
+
+export const skills = [
+	'HTML',
+	'CSS',
+	'Bootstrap',
+	'TailwindCSS',
+	'Sass',
+	'JavaScript',
+	'React',
+	'Next.js',
+	'Node',
+];
