dea93844150a6c10eed25f6c4d06bdd7a8bfe269
Mark <mark@markjwood.com>
2022-10-14 15:55:38 -0700

    add min-width percentage classes for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@ module.exports = {
 	content: ['./src/**/*.{html,js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
+		minWidth: {
+			10: '10%',
+			20: '20%',
+			30: '30%',
+			40: '40%',
+			50: '50%',
+			60: '60%',
+			70: '70%',
+			80: '80%',
+			90: '90%',
+		},
 		extend: {},
 	},
 	variants: {
